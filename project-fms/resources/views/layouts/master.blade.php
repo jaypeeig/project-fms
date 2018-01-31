c215b085b9f4d323f6d5a209e8a3c52063b50d11
jaypeeig <jaypeeig@gmail.com>
2018-01-31 21:11:54 +0800

    Add maps

--- project-fms/resources/views/layouts/master.blade.php
+++ project-fms/resources/views/layouts/master.blade.php
@@ -29,6 +29,8 @@
         @include('partials.header')
         @include('partials.sidebar')
         @yield('content')
+
+        @include('partials.footer')
     </div>
     <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
     <script src="{{ asset('bower_components/jquery-ui/jquery-ui.min.js') }}"></script>
@@ -38,23 +40,15 @@
     <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('bower_components/raphael/raphael.min.js') }}"></script>
     <script src="{{ asset('bower_components/morris.js/morris.min.js') }}"></script>
-    <!-- Sparkline -->
     <script src="{{ asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
-    <!-- jvectormap -->
     <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
     <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-    <!-- jQuery Knob Chart -->
     <script src="{{ asset('bower_components/jquery-knob/dist/jquery.knob.min.js') }}"></script>
-    <!-- daterangepicker -->
     <script src="{{ asset('bower_components/moment/min/moment.min.js') }}"></script>
     <script src="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
-    <!-- datepicker -->
     <script src="{{ asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
-    <!-- Bootstrap WYSIHTML5 -->
     <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-    <!-- Slimscroll -->
     <script src="{{ asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
-    <!-- FastClick -->
     <script src="{{ asset('bower_components/fastclick/lib/fastclick.js') }}"></script>
 
     <script src="{{ asset('js/adminlte.min.js') }}"></script>
