e959ed0d234b64b12ea7189ea78a15fad15a9c25
jaypeeig <jaypeeig@gmail.com>
2018-01-31 20:36:40 +0800

    Update avatar

--- project-fms/resources/views/partials/header.blade.php
+++ project-fms/resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
                 <li class="dropdown user user-menu">
 
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="{{asset('img/user2-160x160.jpg')}}" class="user-image" alt="User Image" />
+                        <img src="{{asset('img/user8-128x128.jpg')}}" class="user-image" alt="User Image" />
                         @guest
                             <span class="hidden-xs"> Guest </span>
                         @else
@@ -25,7 +25,7 @@
                     <ul class="dropdown-menu">
                     <!-- User image -->
                     <li class="user-header">
-                        <img src="{{asset('img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
+                        <img src="{{asset('img/user8-128x128.jpg')}}" class="img-circle" alt="User Image" />
                         <p>
                             {{ Auth::user()->name }} 
                         </p>
--- project-fms/resources/views/partials/sidebar.blade.php
+++ project-fms/resources/views/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
     <section class="sidebar">    
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{asset('img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
+                <img src="{{asset('img/user8-128x128.jpg')}}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
                 <p> {{ Auth::user()->name }} </p>
